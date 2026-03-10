14bb78362a5f6ca63e557e169bde75adbf3fab19
fiqme <afiqamsyar80@gmail.com>
2026-03-10 14:58:27 +0800

    suspense revenue chart

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,17 +2,14 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from "@/app/lib/data";
+import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
   //bila ada async, maksudnya dalam ni akan ada function yang guna await (await: kena tunggu habis fetch baru proceed next line).
 
-  const revenue = await fetchRevenue(); //await akan tunggu sampai data dah habis fetch
-  const latestInvoices = await fetchLatestInvoices();
+  const latestInvoices = await fetchLatestInvoices(); //await akan tunggu sampai data dah habis fetch
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -36,7 +33,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
